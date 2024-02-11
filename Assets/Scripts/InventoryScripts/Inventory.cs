f392f5b72d98c6898a41bb8f9033dbeeba5feb83
Bruno <bruno.v.dede@gmail.com>
2024-02-11 03:02:20 -0500

    Added Enemy Action Scene and Script
    The script is a little messed up, the player only collides/touches the box collider, I need to figure out a way to make the player be able to move ~inside~ the box collider.

--- Assets/Scripts/InventoryScripts/Inventory.cs
+++ Assets/Scripts/InventoryScripts/Inventory.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Misc/EnemyAction.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject playerModel;
+    public BoxCollider areaVisible;
+
+    public float speedOfEnemy = 5;
+
+    bool playerInArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerModel = GameObject.FindGameObjectWithTag("Player");
+        playerInArea = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInArea)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //BoxCollider enemyArea = collision.gameObject.GetComponent<BoxCollider>();
+        if (collision.gameObject == playerModel)
+        {
+            playerInArea = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //BoxCollider enemyArea = collision.gameObject.GetComponent<BoxCollider>();
+        if (collision.gameObject == playerModel)
+        {
+            playerInArea = false;
+        }
+    }
+
+    public void AttackPlayer()
+    {
+        //calculate movement dist
+        var step = speedOfEnemy * Time.deltaTime;
+
+        //basic move towards script
+        transform.position = Vector3.MoveTowards(transform.position, playerModel.transform.position, step);
+    }
+}
