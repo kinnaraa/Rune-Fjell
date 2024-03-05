ae14428ea70b2b6dfe5543ec08eebf4bffdb0daa
kinnaraa <kinnarabosworth@gmail.com>
2024-03-04 23:25:53 -0500

    tried to simplify questLog

--- Assets/Scripts/Quest Log Scripts/QuestLog.cs
+++ Assets/Scripts/Quest Log Scripts/QuestLog.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class QuestLog : MonoBehaviour
 {
-    public List<Quest> quests;
+    public List<Quest> allQuests;
+    public List<Quest> activeQuests;
+    public List<Quest> completedQuests;
 
     public class Quest
     {
         public string title;
-        public Dictionary<Sprite, int> needInfo;
+        public KeyValuePair<Sprite, int> rewardPairs;
         public int numNeeds;
         public string infoText;
-        public bool started = false;
+        public bool isActive = false;
         public bool completed = false;
+        public GameObject questObject;
 
-        public Quest(string questTitle, int needs, string questInfoText)
+        public Quest(string questTitle, int needs, string questInfoText, KeyValuePair<Sprite, int> questRewards)
         {
             title = questTitle;
             infoText = questInfoText;
             numNeeds = needs;
+            rewardPairs = questRewards;
         }
     }
 
@@ -27,29 +34,63 @@ public class QuestLog : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        quests = new List<Quest>()
+        allQuests = new List<Quest>()
         {
-            new Quest("Help the Gnome", 1, "help gnome info...blah blah blah..."),
-            new Quest("Find the Gnome Village", 1, "find gnome village info, blah blah, blah"),
-            new Quest("Good For Nothing Son", 7, "Gnome�s mom needs help getting supplies for dinner"),
-            new Quest("Pesky Wolves", 5, "Gnome is grumpy about the loud wolves in the area and offered you a reward to get rid of them"),
-            new Quest("This Guy Stinks", 10, "Find gnome in town who needs help in weird smokey hut"),
-            new Quest("Cracking the Code", 1, "The seer offered you to try a mushroom, will you accept?"),
-            new Quest("Where Art Gnome", 3, "Several Gnomes have gone missing in the night!"),
-            new Quest("Unknown", 1, "Make your way towards the mountains to try and find the beast who stole the gnomes"),
-            new Quest("Something Lurking in the Deep", 1, "He�s heard legends of a great worm who would eventually awake to destroy gnomekind. Destroy him before he kills us all."),
+            new Quest("Help the Gnome", 1, "In the woods, you hear a voice crying for help in the distance. Navigate the woods, find the voice, and provide aid to the one in trouble.",  new KeyValuePair<Sprite, int>(Resources.Load<Sprite>("UI/Kenaz_Default"), 1)),
+            
+            /*
+            new Quest("Find the Gnome Village", 1, "find gnome village info, blah blah, blah", findVillage),
+            new Quest("Good For Nothing Son", 7, "Gnome's mom needs help getting supplies for dinner", stupidSon),
+            new Quest("Pesky Wolves", 5, "Gnome is grumpy about the loud wolves in the area and offered you a reward to get rid of them", peskyWolves),
+            new Quest("This Guy Stinks", 10, "Find gnome in town who needs help in weird smokey hut", thisGuyStinks),
+            new Quest("Cracking the Code", 1, "The seer offered you to try a mushroom, will you accept?", crackingCode),
+            new Quest("Where Art Gnome", 3, "Several Gnomes have gone missing in the night!", whereArtGnome),
+            new Quest("Unknown", 1, "Make your way towards the mountains to try and find the beast who stole the gnomes", unknownQuest),
+            new Quest("Something Lurking in the Deep", 1, "He�s heard legends of a great worm who would eventually awake to destroy gnomekind. Destroy him before he kills us all.", somethingLurking),
+            */
         };
 
-        for (int i = 0; i < transform.childCount; i++)
+        Debug.Log(allQuests.Count());
+
+        for (int i = 0; i < allQuests.Count; i++)
         {
-            transform.GetChild(i).gameObject.GetComponent<Sprite>();
+            allQuests[i].questObject = gameObject.transform.GetChild(i).gameObject;
+            gameObject.transform.GetChild(i).GetChild(0).gameObject.name = allQuests[i].title;
+            allQuests[i].questObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = allQuests[i].title;
+            allQuests[i].questObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = allQuests[i].infoText;
+            allQuests[i].questObject.transform.GetChild(3).GetComponent<Image>().sprite = allQuests[i].rewardPairs.Key;
+            allQuests[i].questObject.transform.GetChild(5).GetComponent<TextMeshProUGUI>().text = "x" + allQuests[i].rewardPairs.Value;
+            Debug.Log(allQuests[i].rewardPairs.Key + "  " + allQuests[i].rewardPairs.Value);
         }
-        
+
+        allQuests[0].isActive = true;
+
+        activeQuests = new List<Quest>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < allQuests.Count(); i++)
+        {
+            /*if (allQuests[i].isActive && !activeQuests.Contains(allQuests[i]))
+            {
+                activeQuests.Add(allQuests[i]);
+                allQuests[i].questObject.SetActive(true);
+            }
+            else if (!allQuests[i].isActive && activeQuests.Contains(allQuests[i])) 
+            {
+                activeQuests.Remove(allQuests[i]);
+                allQuests[i].questObject.SetActive(false);
+            }else if (!allQuests[i].isActive)
+            {
+                allQuests[i].questObject.SetActive(false);
+            }*/
+
+            if (allQuests[i].isActive)
+            {
+                allQuests[i].questObject.SetActive(true);
+            }
+        }
     }
 }
